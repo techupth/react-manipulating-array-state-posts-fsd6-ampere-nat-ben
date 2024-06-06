4514f0be113fd8f4564a6b44b8989de0a0c4b45d
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-06-06 12:26:48 +0700

    finished assignment

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 function Posts(props) {
   const post = props.post;
-
+  const [counter, setCounter] = useState(post.likes);
+  const handleLikeClick = () => setCounter(counter + 1);
+  const handleDislikeClick = () =>
+    counter <= 0 ? counter : setCounter(counter - 1);
   return (
     <div className="app-wrapper">
       <h1 className="app-title">Posts</h1>
@@ -10,13 +14,17 @@ function Posts(props) {
             <h2>{post.title}</h2>
             <div className="post-social-media-stats">
               <span className="stats-topic">Likes: </span>
-              <span className="post-likes">{post.likes}</span>
+              <span className="post-likes">{counter}</span>
             </div>
           </div>
           <p className="post-content">{post.content}</p>
           <div className="post-actions">
-            <button className="like-button">Like</button>
-            <button className="dislike-button">Dislike</button>
+            <button className="like-button" onClick={handleLikeClick}>
+              Like
+            </button>
+            <button className="dislike-button" onClick={handleDislikeClick}>
+              Dislike
+            </button>
           </div>
         </div>
       </div>
